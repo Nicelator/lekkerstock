41248543f078ee6979a7bf0e685383fa854f4ddf
Nicelator <niceakao@gmail.com>
2026-03-13 20:21:49 +0100

    Fix: map Supabase user to Navbar prop shape

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -5,7 +5,18 @@ export default async function MainLayout({ children }: { children: React.ReactNo
   const user = await getUser();
   return (
     <div className="min-h-screen flex flex-col">
-      <Navbar user={user} />
+      <Navbar
+        user={
+          user
+            ? {
+                id: user.id,
+                email: user.email ?? "",
+                name: user.user_metadata?.full_name ?? user.user_metadata?.name,
+                role: user.user_metadata?.role,
+              }
+            : null
+        }
+      />
       <main className="flex-1">{children}</main>
     </div>
   );
